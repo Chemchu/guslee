2266b9fdfc65371f97b266e47cfae38cf74eea56
gus <gustavo_9_rj@hotmail.com>
2022-01-20 03:47:01 +0100

    portfolio template añadido, framer-motion añadido

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import type { NextPage } from 'next';
+import LandingPage from '../src/components/landingPage';
 const Home: NextPage = () => {
   return (
-    <div>Eyyy</div>
+    <LandingPage />
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
 module.exports = {
-  content: [],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Open Sans"', '"Public Sans"', 'sans-serif', 'system-ui'],
+        mono: ['monospace', 'SFMono-Regular'],
+      },
+      height: {
+      "10v": "10vh",
+      "15v": "15vh",
+      "20v": "20vh",
+      "25v": "25vh",      
+      "30v": "30vh",
+      "35v": "35vh",
+      "40v": "40vh",
+      "45v": "45vh",
+      "50v": "50vh",
+      "55v": "55vh",
+      "60v": "60vh",
+      "65v": "65vh",
+      "70v": "70vh",
+      "75v": "75vh",
+      "80v": "80vh",
+      "85v": "85vh",
+      "90v": "90vh",
+      "95v": "95vh",
+      "100v": "100vh",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
 }
