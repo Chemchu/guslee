b82bfd3d8c424aa9718882f2aa03c4eb83d4d522
gus <gustavo_9_rj@hotmail.com>
2022-01-27 04:16:26 +0100

    añadido dos fondos, a elegir video o foto

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,27 +10,11 @@ module.exports = {
         "outfit-light": ['OutfitLight', 'cursive'],
         "outfit-thin": ['OutfitThin', 'cursive']
       },
-      height: {
-      "10v": "10vh",
-      "15v": "15vh",
-      "20v": "20vh",
-      "25v": "25vh",      
-      "30v": "30vh",
-      "35v": "35vh",
-      "40v": "40vh",
-      "45v": "45vh",
-      "50v": "50vh",
-      "55v": "55vh",
-      "60v": "60vh",
-      "65v": "65vh",
-      "70v": "70vh",
-      "75v": "75vh",
-      "80v": "80vh",
-      "85v": "85vh",
-      "90v": "90vh",
-      "95v": "95vh",
-      "100v": "100vh",
-      },
+      backgroundImage: {
+        'sky': "url('/img/sky.jpg')",
+        'window': "url('/img/window.jpg')",
+        'seta': "url('/img/seta.jpg')",
+      }
     },
   },
   variants: {
