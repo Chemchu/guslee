d7fcd8605630f2291e52753b61daebdeed5c3a97
gus <gustavo_9_rj@hotmail.com>
2022-10-01 03:22:17 +0200

    cambiando la webpage

--- src/components/landingPage/index.tsx
+++ src/components/landingPage/index.tsx
@@ -10,157 +10,34 @@ const LandingPage = () => {
 }
 
 const FondoImagen = () => {
-    const [firstWriterDone, setFirstWriter] = useState<boolean>(false);
-    const [secondWriterDone, setSecondWriter] = useState<boolean>(false);
-    const [thirdWriterDone, setThirdWriter] = useState<boolean>(false);
-
     return (
-        <main className="bg-main bg-cover bg-no-repeat bg-center overflow-x-hidden text-white font-outfit-light h-screen w-screen">
-            <div className="w-full h-full cursor-default">
-                <div className="transform flex justify-around items-center text-base sm:text-lg xl:text-xl">
-                    <div className="transition hover:scale-150 hover:duration-100 cursor-pointer">
-                        Sobre mí
-                    </div>
-
-                    <div className="transition hover:scale-150 hover:duration-100 cursor-pointer">
-                        Trabajos
-                    </div>
-
-                    <div className="transition hover:scale-150 hover:duration-100 cursor-pointer">
-                        Twitter
-                    </div>
-
-                    <div className="transition hover:scale-150 hover:duration-100 cursor-pointer">
-                        Instagram
-                    </div>
-
-                    <div className="transition hover:scale-150 hover:duration-100 cursor-pointer">
-                        GitHub
-                    </div>
-                </div>
-                <hr className="my-2 animate-pulse" />
-                <div className="flex flex-col mt-10 pl-10 lg:mt-0 lg:pl-6 lg:justify-center w-full h-full">
-                    <div className="flex text-xl sm:text-4xl xl:text-6xl">
-                        <div className="font-outfit mr-2 md:mr-4">
-                            {!firstWriterDone ?
-                                <Typewriter
-                                    options={{ delay: 60 }}
-                                    onInit={(typewriter) => {
-                                        typewriter.typeString('¡Hola!')
-                                            .stop()
-                                            .callFunction(() => { setFirstWriter(true) })
-                                            .start();
-                                    }}
-                                />
-                                :
-                                '¡Hola!'
-                            }
-
-                        </div>
-                        <div className="font-outfit-thin">
-                            {firstWriterDone && !secondWriterDone ?
-                                <Typewriter
-                                    options={{ delay: 55 }}
-                                    onInit={(typewriter) => {
-                                        typewriter.typeString('Soy Gus')
-                                            .pauseFor(200)
-                                            .deleteAll()
-                                            .typeString('Me llamo Gustavo Lee')
-                                            .stop()
-                                            .callFunction(() => { setSecondWriter(true) })
-                                            .start();
-                                    }}
-                                />
-                                :
-                                secondWriterDone && 'Me llamo Gustavo Lee'
-                            }
-                        </div>
-                    </div>
-                    <div className="text-lg sm:text-3xl xl:text-5xl my-6">
-                        <div className="flex gap-2 font-outfit-thin mr-2 md:mr-4">
-                            {/* Segundo */}
-                            {secondWriterDone && !thirdWriterDone ?
-                                <Typewriter
-                                    options={{ delay: 40 }}
-                                    onInit={(typewriter) => {
-                                        typewriter.typeString('Soy')
-                                            .pauseFor(400)
-                                            .stop()
-                                            .callFunction(() => { setThirdWriter(true) })
-                                            .start();
-                                    }}
-                                />
-                                :
-                                thirdWriterDone && 'Soy '
-                            }
-                            {thirdWriterDone &&
-                                <Typewriter
-                                    options={{ delay: 70, deleteSpeed: 27, loop: true }}
-                                    onInit={(typewriter) => {
-                                        typewriter
-                                            .typeString('ingeniero de software')
-                                            .pauseFor(1200)
-                                            .deleteChars(22)
-                                            .typeString('guitarrista')
-                                            .pauseFor(700)
-                                            .deleteChars(11)
-                                            .typeString('brasileño')
-                                            .pauseFor(800)
-                                            .deleteChars(9)
-                                            .typeString('informático')
-                                            .pauseFor(900)
-                                            .deleteChars(12)
-                                            .typeString('fullstack developer')
-                                            .pauseFor(1200)
-                                            .deleteChars(19)
-                                            .typeString('panadero')
-                                            .pauseFor(500)
-                                            .deleteChars(8)
-                                            .typeString('gracioso')
-                                            .pauseFor(1200)
-                                            .deleteChars(8)
-                                            .typeString('carismático')
-                                            .pauseFor(950)
-                                            .deleteChars(11)
-                                            .stop()
-                                            .start();
-                                    }}
-                                />
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div >
-
-            <div className="w-full h-full p-4">
-                <Card />
-            </div>
-
-            <div className="w-full h-full bg-green-600">
-                <h1>
-                    Trabajos
-                </h1>
-            </div>
-
-            <div className="w-full h-full bg-cyan-600">
-                <h1>
-                    Twitter
-                </h1>
-            </div>
-
-            <div className="w-full h-full bg-pink-700">
-                <h1>
-                    Instagram
-                </h1>
-            </div>
-
-            <div className="w-full h-full bg-slate-600">
-                <h1>
-                    GitHub
-                </h1>
-            </div>
-
-        </main>
+        <main className="flex justify-center bg-white font-outfit-light h-screen w-screen p-4 sm:p-10 text-white">
+            <motion.div
+                initial={{ opacity: 0, scaleY: 0, y: '50vh' }} animate={{ opacity: 1, scaleY: 1, transition: { duration: 0.8 }, y: '0vh' }}
+                className="bg-gradient-to-r from-blue-400 via-purple-500 to-orange-600  animate-gradient-x
+            flex items-center justify-center w-full h-full
+                text-[20vw] font-outfit-light overflow-clip">
+                <Typewriter
+                    options={{ delay: 70, deleteSpeed: 27, loop: true }}
+                    onInit={(typewriter) => {
+                        typewriter
+                            .typeString('¡Hola!')
+                            .pauseFor(3000)
+                            .deleteAll()
+                            .typeString('Oi!')
+                            .pauseFor(3000)
+                            .deleteAll()
+                            .typeString('Hello!')
+                            .pauseFor(3000)
+                            .deleteAll()
+                            .typeString('Bon dia!')
+                            .pauseFor(3000)
+                            .stop()
+                            .start();
+                    }}
+                />
+            </motion.div>
+        </main >
     );
 }
 
