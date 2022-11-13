9f5578e4defe7b938aabd24509f983003e14dc58
Chemchu <gustavo_9_rj@hotmail.com>
2022-11-13 02:25:18 +0100

    Proyecto cambiado de React (NextJS) a Svelte (SvelteKit)

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next';
-import LandingPage from '../src/components/landingPage';
-const Home: NextPage = () => {
-  return (
-    <LandingPage />
-  )
-}
-
-export default Home
