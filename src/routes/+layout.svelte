9713b3162ca28a7f8c7d86bcd9b6d78ad0478a01
Chemchu <gustavo_9_rj@hotmail.com>
2022-11-13 22:45:13 +0100

    Probando probando

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
 	import '../app.css';
 </script>
 
+<div class="flex w-full justify-end">
+	<nav class="flex flex-wrap justify-evenly bg-transparent w-1/2 h-full text-xl">
+		<a href="/">Sobre mí</a>
+		<a href="/">Proyectos</a>
+		<a href="/">Sobre mí</a>
+		<a href="/">Sobre mí</a>
+	</nav>
+</div>
 <slot />
