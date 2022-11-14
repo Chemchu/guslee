35a55942b495c8381df54054110ed4e0e0d58eba
Chemchu <gustavo_9_rj@hotmail.com>
2022-11-14 19:44:20 +0100

    Fondo añadido. Navbar de cristal también

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,29 @@
 <script lang="ts">
 </script>
 
-<h1 class="text-4xl">Bienvenido!</h1>
+<video class="object-cover fixed -z-10" src="/videos/background_video.mp4" muted loop autoplay>
+	<track kind="captions" />
+</video>
+
+<div class="flex flex-col w-full h-full items-center">
+	<div class="text-white font-merriweather font-thin w-9/12">
+		<div class="flex flex-col w-full">
+			<span class="b-title w-full text-start">Gustavo</span>
+			<span class="m-title w-full text-end">Lee</span>
+		</div>
+	</div>
+	<div class="relative text-white">Coño</div>
+</div>
 
 <style>
+	.b-title {
+		font-size: 20rem;
+	}
+	.m-title {
+		font-size: 16rem;
+	}
+	span {
+		color: white;
+		text-shadow: -1px -1px 0 #999, 1px -1px 0 #999, -1px 1px 0 #999, 1px 1px 0 #999;
+	}
 </style>
