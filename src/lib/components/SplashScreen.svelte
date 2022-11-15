c17d90a858381f7d399e72bb6e299658b3bff17a
Chemchu <gustavo_9_rj@hotmail.com>
2022-11-15 20:00:35 +0100

    Videos añadidos (falta elegir). SplashScreen hecho

---
+++ src/lib/components/SplashScreen.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+</script>
+
+<div class="flex flex-col items-center justify-center w-screen h-screen bg-black z-50" out:fade>
+	<img src="https://media.giphy.com/media/3og0IS6SldW60DdCRa/giphy.gif" alt="Loading GIF" />
+	<span class="text-white font-merriweather text-xl"> Cargando... </span>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,73 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+	import SplashScreen from '$lib/components/SplashScreen.svelte';
+
+	let mounted = false;
+	onMount(async () => {
+		const p = new Promise(() => {
+			setTimeout(() => {
+				mounted = true;
+			}, 2000);
+		});
+
+		await p;
+	});
 </script>
 
-<video class="object-cover fixed -z-10" src="/videos/background_video.mp4" muted loop autoplay>
-	<track kind="captions" />
-</video>
-
-<div class="flex flex-col w-full h-full items-center">
-	<div class="text-white font-merriweather font-thin w-9/12">
-		<div class="flex flex-col w-full">
-			<span class="b-title w-full text-start">Gustavo</span>
-			<span class="m-title w-full text-end">Lee</span>
+{#if !mounted}
+	<SplashScreen />
+{:else}
+	<div class="absolute w-full h-screen bg-black -z-20">
+		<video
+			class="w-full h-full object-cover -z-10 bg-black py-20"
+			src="/videos/background_video5.mp4"
+			muted
+			loop
+			autoplay
+			transition:fade={{ delay: 500, duration: 1000 }}
+		>
+			<track kind="captions" />
+		</video>
+	</div>
+	<div class="flex flex-col w-full h-screen items-center justify-center">
+		<div class="text-white w-10/12 py-10 md:p-16">
+			<div class="flex flex-col w-full" transition:fade={{ delay: 1700, duration: 1500 }}>
+				<span class="s-title w-full text-start">Me llamo</span>
+				<span class="b-title w-full text-center font-extralight">Gustavo</span>
+				<span class="m-title w-full text-end font-bold">Lee</span>
+			</div>
 		</div>
 	</div>
-	<div class="relative text-white">Coño</div>
-</div>
+	<div class="bg-black w-full h-screen">
+		<div class="relative text-white">Coño</div>
+		<div class="relative text-white">Coño</div>
+		<div class="relative text-white">Coño</div>
+		<div class="relative text-white">Coño</div>
+		<div class="relative text-white">Coño</div>
+		<div class="relative text-white">Coño</div>
+		<div class="relative text-white">Coño</div>
+		<div class="relative text-white">Coño</div>
+		<div class="relative text-white">Coño</div>
+		<div class="relative text-white">Coño</div>
+		<div class="relative text-white">Coño</div>
+		<div class="relative text-white">Coño</div>
+	</div>
+{/if}
 
 <style>
 	.b-title {
-		font-size: 20rem;
+		font-size: 16vw;
+		line-height: 100%;
 	}
+
 	.m-title {
-		font-size: 16rem;
+		font-size: 10vw;
+		line-height: 100%;
 	}
-	span {
-		color: white;
-		text-shadow: -1px -1px 0 #999, 1px -1px 0 #999, -1px 1px 0 #999, 1px 1px 0 #999;
+
+	.s-title {
+		font-size: 4vw;
+		line-height: 100%;
 	}
 </style>
