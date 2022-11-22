54164176d1d69aca639f621b9405a8ab4a2a65e8
Chemchu <gustavo_9_rj@hotmail.com>
2022-11-22 22:40:12 +0100

    cambios hechos

--- src/lib/components/AboutMe.svelte
+++ src/lib/components/AboutMe.svelte
@@ -1,12 +1,14 @@
 <script>
 	import InformationCard from './cards/InformationCard.svelte';
-	import { inview } from 'svelte-inview';
 
-	const title = 'Fullstack developer';
-	const msg = 'Graduado en ingeniería informática por la Universitat Politècnica de València';
+	export let id = 'id';
+	const title = 'Un poco de mí';
+	const msg = `Me considero una persona sencilla y agradable.
+	He cursado el grado en ingeniería informática y también el máster en ingeniería y tecnología de sistemas software (MITSS) en la Universitat Politècnica de València.
+	Soy fan de las guitarras, el café y el diseño web.`;
 </script>
 
-<div class="w-screen h-screen bg-transparent text-white p-10" use:inview on:enter on:leave>
+<div {id} class="flex items-center w-screen min-h-screen text-white p-10">
 	<div class="w-full h-96">
 		<InformationCard {title} {msg} />
 	</div>
