1ae31af8002dadb1254ce0d1741ec5f4d91cc414
Chemchu <gustavo_9_rj@hotmail.com>
2024-07-13 12:45:23 +0200

    feat: let's have a fresh start

--- src/lib/components/SplashScreen.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { fade } from 'svelte/transition';
-</script>
-
-<div class="flex flex-col items-center justify-center w-screen h-screen bg-black z-50" out:fade>
-	<img src="https://media.giphy.com/media/B8dUiWJmQi4KCQhS9x/giphy.gif" alt="Loading GIF" />
-	<!-- <span class="text-white font-merriweather text-xl"> Cargando... </span> -->
-</div>
