51657a73d23508b85f0e196d1d7b5afb79fc9bbe
Chemchu <gustavo_9_rj@hotmail.com>
2024-07-14 21:20:07 +0200

    feat: added basic Actix web and Askama site

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files as fs;
+use actix_web::{App, HttpServer};
+
+mod routes;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(fs::Files::new("/_static", "static").show_files_listing())
+            .service(routes::landing_page)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
