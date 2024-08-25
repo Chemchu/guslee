4f4779b6f9b3ca5b290cefc363a2b91a16b4be64
Chemchu <gustavo_9_rj@hotmail.com>
2024-08-26 00:25:41 +0200

    feat: removed animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 module.exports = {
   darkMode: ['selector'],
   content: ["./src/**/*.{html,js}", "./templates/**/*.{html,js}"],
-  theme: {
-    extend: {
-      animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
-      }
-    }
-  },
+  theme: {},
   plugins: [],
 }
