bfe2eeb6b0863d5ad09a3c7a0b0d4f2cea0c74da
Chemchu <gustavo_9_rj@hotmail.com>
2024-09-09 00:40:52 +0200

    feat: localization basic structure done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use actix_files as fs;
 use actix_web::{middleware::Logger, App, HttpServer};
 use env_logger::Env;
+use i18n::translator::Language;
 
 mod i18n;
 mod routes;
@@ -8,6 +9,13 @@ mod routes;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    let translator = i18n::translator::Translator::new();
+    let en = translator.get_translation("test1", &Language::English);
+    let es = translator.get_translation("test1", &Language::Spanish);
+    let pt = translator.get_translation("test1", &Language::Portuguese);
+
+    println!("All languages {:?}, {:?} and {:?}", en, es, pt);
 
     HttpServer::new(|| {
         App::new()
