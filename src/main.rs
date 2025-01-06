523855eebacaa3e9ab365560f9252a69743ed329
Chemchu <gustavo_9_rj@hotmail.com>
2025-01-06 04:18:48 +0100

    feat(WIP): adding http client

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod i18n;
 mod routes;
 
 pub struct AppState {
+    http_caller: reqwest::Client,
     supabase_url: String,
 }
 
@@ -13,12 +14,16 @@ pub struct AppState {
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    dotenv::dotenv().ok().expect("Missing env variables");
+    dotenv::dotenv().expect("Missing env variables");
+    let client = reqwest::Client::builder()
+        .build()
+        .expect("Error creating HTTP client");
 
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
             .app_data(Data::new(AppState {
+                http_caller: client,
                 supabase_url: std::env::var("SUPABASE_URL").expect("Missing env SUPABASE_URL"),
             }))
             .service(fs::Files::new("/_static", "static").show_files_listing())
