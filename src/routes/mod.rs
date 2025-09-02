a9987155d6877860f4ef5a7f69c2103a2bc2b1e1
Chemchu <gustavo_9_rj@hotmail.com>
2025-09-02 23:34:11 +0100

    feat: added welcome md and fixed landing page

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -4,44 +4,25 @@ use actix_web::{
 };
 use std::{fs, io};
 
-/* fn get_markdown_options() -> Options {
-    Options {
-        parse: ParseOptions::gfm(), // GitHub Flavored Markdown
-        compile: CompileOptions {
-            allow_dangerous_html: true,
-            allow_dangerous_protocol: false,
-            ..CompileOptions::default()
-        },
-    }
-} */
-
 #[get("/")]
 pub async fn landing() -> impl Responder {
-    let path = "./templates/index.html";
-    let content: String =
-        fs::read_to_string(path).unwrap_or_else(|_| panic!("File not found: {}", path));
+    let content: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/garden/welcome.md"));
 
-    Html::new(content)
+    Html::new(wrap_markdown_with_whole_page(&markdown::to_html(content)))
 }
 
 #[get("/{post:.*}")]
 pub async fn post(req: HttpRequest, route: web::Path<String>) -> impl Responder {
     let content: io::Result<String> = fs::read_to_string(format!("./garden/{}.md", route));
 
-    /*     let options = get_markdown_options(); */
-
     let is_htmx_req = req.headers().get("HX-Request").is_some();
     if is_htmx_req {
         Html::new(match content {
-            /*             Ok(md) => markdown::to_html_with_options(&md, &options).unwrap(), */
             Ok(md) => markdown::to_html(&md),
             Err(_err) => String::from("Fallback page"),
         })
     } else {
         Html::new(match content {
-            /* Ok(md) => wrap_markdown_with_whole_page(
-                &markdown::to_html_with_options(&md, &options).unwrap(),
-            ), */
             Ok(md) => wrap_markdown_with_whole_page(&markdown::to_html(&md)),
             Err(_err) => String::from("Fallback page"),
         })
@@ -49,9 +30,7 @@ pub async fn post(req: HttpRequest, route: web::Path<String>) -> impl Responder 
 }
 
 fn wrap_markdown_with_whole_page(content: &str) -> String {
-    let path = "./templates/index.html";
-    let html: String =
-        fs::read_to_string(path).unwrap_or_else(|_| panic!("File not found: {}", path));
+    let html: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/index.html"));
 
     html.replace("{{CONTENT}}", content)
 }
