8e1cb662b60d72114936b33954ec3a27d0e19e7f
Chemchu <gustavo_9_rj@hotmail.com>
2025-10-07 01:58:50 +0100

    feat: fixed colors and changed fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,33 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./templates/**/*.{html,js}"],
   theme: {
     colors: {
-     'text-color': '#979dac',
-     'background-color': '#33415c',
-     'primary-color': '#bc6c25',
+     'text-color': '#DBDFE5',
+     'background-color': '#1E242C',
+     'primary-color': '#F58A07',
      'secondary-color': '#bc6c25',
      'accent-color': '#bc6c25',
-     'shade-color': '#606c38',
+     'shade-color': '#323C49',
     },
     extend: {
       typography: () => ({
         theme: {
           css: {
-            // Light mode colors
-            "--tw-prose-body": "#979dac", // Main text
-            "--tw-prose-headings": "#979dac", // Headings
-            "--tw-prose-lead": "#979dac", // Lead text
-            "--tw-prose-links": "#023e7d", // Links
-            "--tw-prose-bold": "#979dac", // Bold text
+            h1: {
+              fontSize: "2.5em"
+            },
+            h2: {
+              fontSize: "2em"
+            },
+            "--tw-prose-headings": "#DBDFE5", // Headings
+            "--tw-prose-body": "#DBDFE5", // Main text
+            "--tw-prose-lead": "#DBDFE5", // Lead text
+            "--tw-prose-bold": "#DBDFE5", // Bold text
+            "--tw-prose-links": "#F58A07", // Links
             "--tw-prose-counters": "#00ADB5", // counters
             "--tw-prose-bullets": "#00ADB5", // Bullets
             "--tw-prose-hr": "#cad2c5", // Horizontal rules
-            "--tw-prose-quotes": "#023e7d", // Quotes
-            "--tw-prose-quote-borders": "#84a98c", // Quote borders
+            "--tw-prose-quotes": "#DBDFE5", // Quotes
+            "--tw-prose-quote-borders": "#F58A07", // Quote borders
             "--tw-prose-captions": "#52796f", // Captions
             "--tw-prose-code": "#354f52", // Inline code
             "--tw-prose-pre-code": "#cad2c5", // Code in pre blocks
