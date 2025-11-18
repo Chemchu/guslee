3ac330e4dc8a2b4dcee70bc24535f830259c61c3
Chemchu <gustavo_9_rj@hotmail.com>
2025-11-18 23:18:27 +0000

    feat: refactored some function to make use of htmx params ext

--- src/routes/graph_routes.rs
+++ src/routes/graph_routes.rs
@@ -1,30 +1,19 @@
 use actix_web::{
-    HttpRequest, get,
+    get,
     web::{self, Html},
 };
 use maud::html;
 
 use crate::routes::AppState;
 
-#[get("/graph")]
-pub async fn graph_network(app_state: web::Data<AppState>, req: HttpRequest) -> Html {
-    let current_url = req.headers().get("HX-Current-URL");
-    let graph_data = if let Some(current_url) = current_url {
-        let result: Vec<&str> = current_url.to_str().unwrap().splitn(4, '/').collect();
-        let file_name = result
-            .get(3)
-            .filter(|s| !s.is_empty())
-            .unwrap_or(&"welcome");
-        let file_path = format!("{}.md", file_name);
+#[get("/graph/{current_url_pathname}")]
+pub async fn graph_network(app_state: web::Data<AppState>, path: web::Path<String>) -> Html {
+    let graph_data = {
+        let file_path = format!("{}.md", path.as_str());
         app_state
             .search_engine
             .get_graph_from_related_posts(&file_path)
             .await
-    } else {
-        search_engine::types::GraphData {
-            nodes: vec![],
-            edges: vec![],
-        }
     };
 
     let nodes_json = serde_json::to_string(&graph_data.nodes).unwrap();
