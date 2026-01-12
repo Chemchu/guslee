7598dc679ca8477720dda974b28690abc83bafb7
Chemchu <gustavo_9_rj@hotmail.com>
2026-01-12 00:41:46 +0000

    feat: added loading .env from system

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -21,6 +21,20 @@ pub fn read_env_file() -> HashMap<String, String> {
             }
             env_vars
         }
-        None => env_vars,
+        None => load_vars_from_environment(),
     }
+}
+
+// This function tries to load the environment variables already present in the system.
+// It is useful if the environment in which the app is deployed does not have a .env file defined
+fn load_vars_from_environment() -> HashMap<String, String> {
+    let mut env_vars: HashMap<String, String> = HashMap::new();
+
+    let lichess_api_token = std::env::var("LICHESS_API_TOKEN").unwrap();
+    let lichess_username = std::env::var("LICHESS_USERNAME").unwrap();
+
+    env_vars.insert("LICHESS_API_TOKEN".to_string(), lichess_api_token);
+    env_vars.insert("LICHESS_USERNAME".to_string(), lichess_username);
+
+    env_vars
 }
